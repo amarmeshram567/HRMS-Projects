20ef99dd0916c266147f30bac53ec9546c6beabe
amarmeshram567 <meshramamar567@gmail.com>
2025-11-23 23:42:05 +0530

    fix

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,3 +1,20 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: 'postgres',
+//         logging: false
+//     }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -9,18 +26,14 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false  // important for Render Postgres
+            }
+        }
     }
 );
 
 module.exports = sequelize;
-
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.DB_HOST, {
-//     dialect: 'postgres',
-//     logging: false,
-// });
-
-// module.exports = sequelize;
