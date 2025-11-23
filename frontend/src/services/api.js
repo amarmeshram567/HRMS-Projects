0054384155ff8503d9735629c896739fd108015c
amarmeshram567 <meshramamar567@gmail.com>
2025-11-23 23:51:54 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_BASE || 'http://localhost:5000/api',
+    baseURL: import.meta.env.REACT_APP_API_BASE || 'https://hrms-projects-1.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
